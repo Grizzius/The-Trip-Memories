d94ce20048318ce43ac3d459012ee44fc8df1fb0
Alexandre <alex795.fau@gmail.com>
2022-03-11 12:36:28 +0100

    2022.03.11

--- Assets/Scripts/SO/RandomEventData.cs
+++ Assets/Scripts/SO/RandomEventData.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
 [CreateAssetMenu(fileName = "Random Event data", menuName = "Create new random event data")]
 public class RandomEventData : ScriptableObject
@@ -8,4 +10,41 @@ public class RandomEventData : ScriptableObject
     public string eventText;
 
     public int moneyModifier;
+    public int luckModifier;
+    public int knowledgeModifier;
+
+    [Header("Conditions")]
+    public int moneyCondition;
+    public Day[] openDays;
+    public List<Trigger> triggers;
+
+    public ProbabilityType probabilityType;
+    public AnimationCurve curve;
+}
+
+public enum ProbabilityType
+{
+    Default,
+    Luck,
+    Knowledge
+}
+
+public enum EventTriggerType
+{
+    Cancels,
+    Necessary
+}
+
+[System.Serializable]
+public class Trigger
+{
+    public EventTriggerType triggerType;
+    public string triggerName;
+}
+
+[System.Serializable]
+public class EventTrigger 
+{
+    public string triggerName;
+    public bool triggerState;
 }
